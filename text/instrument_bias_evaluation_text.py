11142653135f48fecbf59754959d0f5cdca90a22
mohammadrad81 <mohammad.rad81@gmail.com>
2025-12-18 12:39:41 +0330

    feat: append to partial result in batched

--- text/instrument_bias_evaluation_text.py
+++ text/instrument_bias_evaluation_text.py
@@ -42,11 +42,14 @@ def evaluate_llms(model_names: list[str], max_new_tokens: int=100, batch_size: i
     for model_name in model_names:
         pipe = load_pipeline(model_name)
         output_dataset_path = model_name.replace("/", "__").replace(".", "_") + ".json"
+        partial_result: pd.DataFrame = None
         data_texts = []
         data_instruments = []
         data_genders = []
         data_is_with_reasons = []
         data_prompts = []
+        if os.path.isfile(output_dataset_path):
+            partial_result = pd.read_json(output_dataset_path)
         for text in texts:
             for social_bias_gender in GENDERS:
                 for instrument in CATEGORIES[social_bias_gender]:
@@ -54,13 +57,15 @@ def evaluate_llms(model_names: list[str], max_new_tokens: int=100, batch_size: i
                         (False, WITHOUT_REASON_PROMPT_TEMPLATE),
                         (True, WITH_REASON_PROMPT_TEMPLATE),
                     ]:
-                        
+                        complete_prompt = template_to_complete_prompt(prompt_template, text, instrument, question)
+                        if partial_result and complete_prompt not in partial_result["prompt"]:
+                            continue
                         data_texts.append(text)
                         data_instruments.append(instrument)
                         data_genders.append(social_bias_gender)
                         data_is_with_reasons.append(is_with_reason)
-                        data_prompts.append(template_to_complete_prompt(prompt_template, text, instrument, question))
-        total_length = len(prompt_dataset)
+                        data_prompts.append()
+        total_length = len(data_prompts)
         print("inference started")
         start_time = dt.datetime.now()
         prompt_dataset = Dataset.from_list(
@@ -77,14 +82,16 @@ def evaluate_llms(model_names: list[str], max_new_tokens: int=100, batch_size: i
             for out in pipeline_outputs
         ]
         output_dataset = pd.DataFrame({
-            "text": data_texts[:total_length],
-            "instrument": data_instruments[:total_length],
-            "gender": data_genders[:total_length],
-            "is_with_reason": data_is_with_reasons[:total_length],
-            "prompt": data_prompts[:total_length],
-            "model_output": data_model_outputs[:total_length],
+            "text": data_texts,
+            "instrument": data_instruments,
+            "gender": data_genders,
+            "is_with_reason": data_is_with_reasons,
+            "prompt": data_prompts,
+            "model_output": data_model_outputs,
         })
-        output_dataset.to_json(output_dataset_path)
+        if partial_result:
+            output_dataset = pd.concat([partial_result, output_dataset])
+        output_dataset.to_json(output_dataset_path, index=False)
         end_time = dt.datetime.now()
         print("inference finished, total time: ", str(end_time - start_time))
         print("removing model from GPU...")
